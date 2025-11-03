a9020e4314a097971260acdd691244face7f721d
NetoDog <hilariocancineto@gmail.com>
2025-11-03 10:41:45 -0300

    up

--- PedidosVenda/PedidoInternacional.cs
+++ PedidosVenda/PedidoInternacional.cs
@@ -7,14 +7,10 @@ namespace PedidosVenda
         public PedidoInternacional(Func<decimal, decimal>? frete = null, Func<decimal, decimal>? promocao = null)
             : base(frete, promocao) { }
 
-        protected override void Validar()
-        {
-            Console.WriteLine("Validando pedido internacional...");
-        }
+        protected override void Validar() => Console.WriteLine("Validando pedido internacional...");
 
         protected override decimal CalcularSubtotal() => 200m;
 
-        protected override string EmitirRecibo(decimal total)
-            => $"Commercial Invoice: Total USD {total:C}";
+        protected override string EmitirRecibo(decimal total) => $"Commercial Invoice: Total USD {total:C}";
     }
 }
